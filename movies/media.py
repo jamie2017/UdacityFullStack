7d471270f3297981afef065960f3f8c68ad7b492
JM-YE <yjm.code@gmail.com>
2017-08-11 16:18:19 -0700

    update1

--- movies/media.py
+++ movies/media.py
@@ -5,9 +5,18 @@
 @file: media.py
 @time: 8/11/17 3:32 PM
 """
+import webbrowser
 
 
 class Movie(object):
-    def __init__(self):
-        pass
+    def __init__(self,movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
+
 
