6f98c340a1703d6387211dda980b48b1226e7405
JM-YE <yjm.code@gmail.com>
2017-08-11 17:38:59 -0700

    Beta1: Movie Trailer Website

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -18,9 +18,30 @@ avatar = media.Movie("Avatar",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
+game_of_thrones = media.TvShow("Game of Thrones",
+                               "An American fantasy drama television series created by David Benioff and D. B. Weiss.",
+                               "https://upload.wikimedia.org/wikipedia/en/0/04/Game_of_Thrones_Season_7.jpg",
+                               "https://www.youtube.com/watch?v=giYeaKsXnsI",
+                               7,
+                               4,
+                               "HBO")
 
-# movies = [toy_story,avatar]
-# fresh_tomatoes.open_movies_page(movies)
+secret_forest = media.TvShow("Stranger",
+                             "a South Korean television series starring Jo Seung-woo and Bae Doo-na.",
+                             "https://upload.wikimedia.org/wikipedia/en/d/d8/Stranger_Poster.jpg",
+                             "https://youtu.be/xUOSR7GDCqY",
+                             1,
+                             2,
+                             "tvN")
 
-print media.Movie.valid_rating
-print media.Movie.__doc__ #  >> This class provides a way to store movie related information
+
+videos = [toy_story,avatar,game_of_thrones,secret_forest]
+fresh_tomatoes.open_movies_page(videos)
+
+# print media.Movie.valid_rating
+# print media.Movie.__doc__ #  >> This class provides a way to store movie related information
+# print media.Movie.__module__ # media
+# print media.Movie.__name__   # Movie
+
+
+# game_of_thrones.show_trailer()
--- movies/media.py
+++ movies/media.py
@@ -7,12 +7,19 @@
 """
 import webbrowser
 
+class Video():
+    def __init__(self, title, duration):
+        print "Video Constructor is called."
+        self.title = title
+        self.duration = duration
 
-class Movie(object):
+
+class Movie(Video):
     """ This class provides a way to store movie related information"""
     valid_rating = ["G","PG","PG-13","R"] # Class Variable
 
     def __init__(self,movie_title, movie_storyline, poster_image, trailer_youtube):
+        print "Movie Constructor is called."
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
@@ -23,3 +30,14 @@ class Movie(object):
         webbrowser.open(self.trailer_youtube_url)
 
 
+
+class TvShow(Movie):
+    def __init__(self,movie_title, movie_storyline, poster_image, trailer_youtube,season, episode, tv_station):
+        print "TvShow Constructor is called."
+        Movie.__init__(self,movie_title, movie_storyline, poster_image, trailer_youtube)
+        self.season = season
+        self.episode = episode
+        self.tv_station = tv_station
+
+    def get_local_listing(self):
+        pass
